0d63274cea9129e860f723fd9030196e5465b8b1
Pineapple4122 <936520805@qq.com>
2022-04-07 20:23:31 +0800

    Home

--- sph/src/App.vue
+++ sph/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <Header></Header>
+    <router-view></router-view>
+    <Footer v-show="$route.meta.show"></Footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
--- sph/src/main.js
+++ sph/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//三级联动组件---全局组件
+import TypeNav from '@/pages/Home/TypeNav'
+//第一个参数：全局组件的名字 第二个参数：哪一个组件
+Vue.component(TypeNav.name,TypeNav)
+
+import router from '@/router'
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
