f83c331cfad00bbf32982631b6dc76bd9614f13a
pineApple <936520805@qq.com>
2022-06-11 21:36:07 +0800

    heima

--- shop_heima/src/main.js
+++ shop_heima/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入全局样式表
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
