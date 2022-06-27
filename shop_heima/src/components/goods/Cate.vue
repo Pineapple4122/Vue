ec42ac07f2e3967e6b9d799d7670fddea52fe762
Pineapple4122 <936520805@qq.com>
2022-06-27 20:52:25 +0800

    Cate

--- shop_heima/src/components/goods/Cate.vue
+++ shop_heima/src/components/goods/Cate.vue
@@ -44,21 +44,18 @@
     <el-dialog
       title="添加分类"
       :visible.sync="addCateDialogVisible"
-      width="50%">
+      width="50%" @close="addCateDialogClosed">
       <el-form :model="addCateForm" ref="addCateFormRef"
        label-width="100px" :rules="addCateFormRules">
          <el-form-item label="分类名称" prop="cat_name">
             <el-input v-model="addCateForm.cat_name"></el-input>
          </el-form-item>
          <el-form-item label="父级分类">
-            <el-select v-model="addCateForm.cat_pid" placeholder="">
-               <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
-               </el-option>
-            </el-select>
+            <el-cascader
+               v-model="selectedKeys"
+               :options="parentCateList"
+               :props="{ expandTrigger: 'hover', ...cascaderProps, checkStrictly:'true' }"
+               @change="parentCateChanged" clearable></el-cascader>
          </el-form-item>
       </el-form>
       <div slot="footer">
@@ -112,7 +109,14 @@ export default {
             cat_name: [
                {required: true, message:'请输入分类名称', trigger: 'blur'},
             ],
-         }
+         },
+         parentCateList: [],
+         cascaderProps: {
+            value: 'cat_id',
+            label: 'cat_name',
+            children: 'children'
+         },
+         selectedKeys: [],
       }
    },
    created() {
@@ -136,14 +140,44 @@ export default {
          this.getCateList()
       },
       showAddCateDialog(){
+         this.getParentCateList()
          this.addCateDialogVisible = true
-      }
-   },
+      },
+      async getParentCateList(){
+         const {data:res} = await this.$http.get('categories',{
+            params: {type:2}
+         })
+         if(res.meta.status !== 200){
+            return this.$message.error('获取父级分类数据失败！')
+         }
+         this.parentCateList = res.data
+      },
+      parentCateChanged(){
+         if(this.selectedKeys.length>0){
+            this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length-1]
+            this.addCateForm.cat_level = this.selectedKeys.length
+            return
+         }else{
+            this.addCateForm.cat_pid = 0
+            this.addCateForm.cat_level = 0
+         }
+      },
+      addCateDialogClosed(){
+         this.$refs.addCateFormRef.resetFields()
+         this.selectedKeys = []
+         this.addCateForm.cat_pid = 0
+         this.addCateForm.cat_level = 0
+      },
+   }
 }
 </script>
 
 <style lang="less" scoped>
 .treeTable{
    margin-top: 15px;
+}
+
+.el-cascader{
+   width: 100%;
 }
 </style>
