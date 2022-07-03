0f9034335682c864328e64ad2c2b909d2fb3cdd9
Pineapple4122 <936520805@qq.com>
2022-07-03 17:37:14 +0800

    Params

--- shop_heima/src/components/goods/Add.vue
+++ shop_heima/src/components/goods/Add.vue
@@ -9,11 +9,54 @@
       <el-card>
          <el-alert title="添加商品信息" type="info" center show-icon :closable="false">
          </el-alert>
-         <el-steps :active="1" finish-status="success" :space="200">
-            <el-step title="已完成"></el-step>
-            <el-step title="进行中"></el-step>
-            <el-step title="步骤 3"></el-step>
+         <el-steps :active="activeIndex-0" finish-status="success" :space="200" align-center >
+            <el-step title="基本信息"></el-step>
+            <el-step title="商品参数"></el-step>
+            <el-step title="商品属性"></el-step>
+            <el-step title="商品图片"></el-step>
+            <el-step title="商品内容"></el-step>
+            <el-step title="完成"></el-step>
          </el-steps>
+
+         <el-form ref="addFormRef" :model="addForm" label-width="100px"
+          :rules="addFormRules" label-position="top">
+            <el-tabs v-model="activeIndex" tab-position="left"
+             :before-leave="beforeTabLeave" @tab-click="tabClicked">
+               <el-tab-pane label="基本信息" name="0">
+                  <el-form-item label="商品名称" prop="goods_name">
+                     <el-input v-model="addForm.goods_name"></el-input>
+                  </el-form-item>
+                  <el-form-item label="商品价格" prop="goods_price" type="number">
+                     <el-input v-model="addForm.goods_price"></el-input>
+                  </el-form-item>
+                  <el-form-item label="商品重量" prop="goods_weight" type="number">
+                     <el-input v-model="addForm.goods_weight"></el-input>
+                  </el-form-item>
+                  <el-form-item label="商品数量" prop="goods_number" type="number">
+                     <el-input v-model="addForm.goods_number"></el-input>
+                  </el-form-item>
+                  <el-form-item label="商品分类" prop="goods_cat">
+                     <el-cascader
+                        v-model="addForm.goods_cat"
+                        :options="cateList"
+                        :props="{ expandTrigger: 'hover', ...cateProps, checkStrictly:'true' }"
+                        @change="handleChange" clearable></el-cascader>
+                  </el-form-item>
+               </el-tab-pane>
+               <el-tab-pane label="商品参数" name="1">
+                  <el-form-item :label="item.attr_name"
+                   v-for="(item,i) in manyTableData" :key="item.attr_id">
+                     <el-checkbox-group v-model="item.attr_vals">
+                        <el-checkbox label=""
+                         v-for="(cb,i) in item.attr_vals" :key="i"></el-checkbox>
+                     </el-checkbox-group>
+                  </el-form-item>
+               </el-tab-pane>
+               <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+               <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+               <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+            </el-tabs>
+         </el-form>
       </el-card>
   </div>
 </template>
@@ -21,6 +64,84 @@
 <script>
 export default {
    name: 'Add',
+   data() {
+      return {
+         activeIndex: '0',
+         addForm: {
+            goods_name: '',
+            goods_price: 0,
+            goods_weight: 0,
+            goods_number: 0,
+            goods_cat: []
+         },
+         addFormRules: {
+            goods_name: [{required: true, message:'请输入商品名称', trigger: 'blur'}],
+            goods_price: [{required: true, message:'请输入商品价格', trigger: 'blur'}],
+            goods_weight: [{required: true, message:'请输入商品重量', trigger: 'blur'}],
+            goods_number: [{required: true, message:'请输入商品数量', trigger: 'blur'}],
+            goods_cat: [{required: true, message:'请选择商品分类', trigger: 'blur'}],
+         },
+         cateList: [],
+         cateProps: {
+            label: 'cat_name',
+            value: 'cat_id',
+            children: 'children'
+         },
+         manyTableData: [],
+      }
+   },
+   created() {
+      this.getCateList()
+   },
+   methods: {
+      async getCateList(){
+         const {data:res} = await this.$http.get('categories')
+         if(res.meta.status !== 200){
+            return this.$message.error('获取商品分类列表失败！')
+         }
+         this.cateList = res.data
+      },
+      handleChange(){
+         if(this.addForm.goods_cat.length !== 3){
+            this.addForm.goods_cat = []
+         }
+      },
+      beforeTabLeave(activeName,oldActiveName){
+         // this.$refs.addFormRef.validate(valid => {
+         //    if(!valid){
+         //       this.$message.error('请先完善表单！')
+         //       return false
+         //    }
+         // })
+         if(oldActiveName === '0' && this.addForm.goods_cat.length !== 3){
+            this.$message.error('请先选择商品分类！')
+            return false
+         }
+      },
+      async tabClicked(){
+         if(this.activeIndex === '1'){
+            const {data:res} = await this.$http.get(`categories/${this.cateId}/attributes`,{
+               params: {sel: 'many'}
+            })
+            if(res.meta.status !== 200){
+               return this.$message.error('获取动态参数失败！')
+            }
+            res.data.forEach(item => {
+               item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.split(',')
+            });
+            this.manyTableData = res.data
+            // this.manuTableData = JSON.parse(JSON.stringify(res.data))
+         }
+      }
+   },
+   computed: {
+      cateId(){
+         if(this.addForm.goods_cat.length === 3){
+            return this.addForm.goods_cat[2]
+         }
+         return null
+      }
+   }
 }
 </script>
 
